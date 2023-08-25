9c949301bb8719338d02e81b1f2cffbe53eb2ed5
VSRahul789 <sairahul.k64@gmail.com>
2023-08-25 15:36:57 +0530

    Reamaining files added

--- project3/src/ColorModeSwitcher.js
+++ project3/src/ColorModeSwitcher.js
@@ -4,17 +4,16 @@ import { FaMoon, FaSun } from 'react-icons/fa';
 
 export const ColorModeSwitcher = props => {
   const { toggleColorMode } = useColorMode();
-  const text = useColorModeValue('dark', 'light');
   const SwitchIcon = useColorModeValue(FaMoon, FaSun);
 
   return (
     <IconButton
-      size="md"
-      fontSize="lg"
-      aria-label={`Switch to ${text} mode`}
-      variant="ghost"
       color="current"
-      marginLeft="2"
+      variant="ghost"
+      right={4}
+      top={4}
+      zIndex={'overlay'}
+      pos={'fixed'}
       onClick={toggleColorMode}
       icon={<SwitchIcon />}
       {...props}
